b206d731fa666e4fa5763eacd7e8422e0559d341
J Rose <josh@upros3.com>
2018-03-01 20:00:58 +0000

    relooked at categories exercise and added list of cats

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < ApplicationController
   def index
-    @category = Category.order(id: :desc).all
+    @categories = Category.order(id: :desc).all
   end
 
   def new
@@ -8,18 +8,24 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def create
-    @category = Category.new(product_params)
+    @category = Category.new(category_params)
 
     if @category.save
-      redirect_to %i[admin products], notice: 'Category created!'
+      redirect_to %i[admin category], notice: 'Category created!'
     else
       render :new
     end
   end
-  
+
+  def destroy
+    @category = Category.find params[:id]
+    @category.destroy
+    redirect_to [:admin, :categories], notice: 'Category deleted!'
+  end
+
   private
 
-  def product_params
+  def category_params
     params.require(:category).permit(
       :name
     )
