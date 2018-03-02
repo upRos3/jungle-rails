63e893eb63ec0768a36108dbc40b69ec968efaeb
J Rose <josh@upros3.com>
2018-03-02 22:30:39 +0000

    added routing for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  resources :products, only: [:index, :show]
+  resources :products, only: [:index, :show] do
+    resource :rating, only: [:create, :update, :destroy]
+  end
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
