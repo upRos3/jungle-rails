8b5b7b812728df0c93bb642caabd20427daf324e
J Rose <josh@upros3.com>
2018-03-03 22:11:35 +0000

    fixed code so people can still buy items without logging in, without causing errors

--- app/mailers/receipt.rb
+++ app/mailers/receipt.rb
@@ -4,6 +4,10 @@ class Receipt < ApplicationMailer
   def receipt_email(current_user, order)
     @user = current_user
     @order = order
-    mail(to: @user.email, subject: "Thank you for order ##{@order.id}!")
+    if current_user
+      mail(to: @user.email, subject: "Thank you for order ##{@order.id}!")
+    else
+      mail(to: "generic@fakeeemail.com", subject: "Thank you for order ##{@order.id}!")
+    end
   end
 end
